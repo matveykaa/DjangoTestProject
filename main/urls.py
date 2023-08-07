2a179750bce4f02a997f71bb04f0b7bcb7a63347
Aliona Matweyenko <matveenkoalena2844@mail.ru>
2023-08-07 14:40:11 +0300

    FIx

--- main/urls.py
+++ main/urls.py
@@ -2,9 +2,9 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index),
-    path('aboutus', views.aboutus),
-    path('create', views.create),
+    path('', views.index, name='home'),
+    path('aboutus', views.aboutus, name='aboutus'),
+    path('create', views.create, name='create'),
     path('delete/<int:task_id>/', views.delete, name='delete'),
     path('update/<int:task_id>/', views.update, name='update'),
 ]
